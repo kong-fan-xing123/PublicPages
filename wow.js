118c9cbfba7b154e3860a1b5d8c44cf349515807
kong-fan-xing123 <kfstary@outlook.com>
2021-01-27 16:13:32 +0800

    Update wow.js

--- wow.js
+++ wow.js
@@ -1,10 +1,12 @@
-function getpwd(len){
-  chr=[]
-    for (i=32;i<127;i++){
-        chr.push(String.fromCharCode(i))
-    };var o="",k;for(i=0;i<len;i++){
-        k=Math.floor(Math.random()*chr.length);console.log(k)
-        o+=chr[k]
-    }
-    return {pwd:o,chrs:chr};
+function getpwd(len, debug) {
+ chr = []
+ for (i = 32; i < 127; i++) {
+  chr.push(String.fromCharCode(i))
+ }; var o = "", k;
+ for (i = 0; i < len; i++) {
+  k = Math.floor(Math.random() * chr.length);
+  if (debug) { console.log(k) }
+  o += chr[k]
+ }
+ return { pwd: o, chrs: chr };
 }
